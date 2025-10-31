9cd12a4465d86bfd2f7f3731b6e7bc12f209df4b
RobertoNovelo12 <robertonovelo98@gmail.com>
2025-10-31 13:21:33 -0500

    página principal

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITSFCP/PROYECTOS</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+    <div class='menu'>
+        <div class="header">
+            <div class="logo">ITSFCP/PROYECTOS</div>
+            <div class="header-right">
+                <input type="text" class="search-box" placeholder="Buscar">
+                <button class="login-btn">Iniciar sesión</button>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="sidebar">
+                <div class="menu-item active">
+                    <span class="menu-icon"><img src="./icons/home.svg" alt="" srcset=""></span>
+                    <span>Inicio</span>
+                </div>
+                <div class="menu-item">
+                    <span class="menu-icon"><img src="./icons/proyects.svg" alt="" srcset=""></span>
+                    <span>Proyectos</span>
+                </div>
+                <div class="menu-item">
+                    <span class="menu-icon"><img src="./icons/iconoir--learning.svg" alt="" srcset=""></span>
+                    <span>Conocer más</span>
+                </div>
+                <div class="border-top"></div>
+                <div class="menu-item">
+                    <span class="menu-icon"><img src="./icons/documentation.svg" alt="" srcset=""></span>
+                    <span>Documentación</span>
+                </div>
+                <div class="menu-bottom">
+                    <div class="support-menu">
+                        <div class="menu-item">
+                            <span class="menu-icon"><img src="./icons/support.svg" alt="" srcset=""></span>
+                            <span>Soporte</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="main-content">
+                <h1 class="title">Sistema web responsive para gestión de proyectos en Instituto Tecnológico Superior
+                    Felipe Carrillo Puerto</h1>
+
+                <div class="content-section">
+                    <div class="text-content">
+                        <p>Esta plataforma ha sido desarrollada para optimizar la <strong>administración, monitoreo y
+                                control de proyectos</strong> en el Instituto Tecnológico Superior de Felipe Carrillo
+                            Puerto.</p>
+
+                        <p>Su principal objetivo es <strong>centralizar el registro y seguimiento de los proyectos
+                                académicos</strong>, especialmente los de investigación, ofreciendo un entorno
+                            accesible, organizado y eficiente.</p>
+
+                        <p>A través de este sistema, es posible <strong>registrar información, generar reportes por
+                                periodo, exportar datos en formato PDF</strong> y <strong>consultar en tiempo real el
+                                estado de los proyectos en ejecución.</strong></p>
+
+                        <p>Con ello, se promueve una <strong>gestión más ágil y sustentable</strong>, reduciendo el uso
+                            de documentos físicos, mejorando la comunicación institucional y fortaleciendo los procesos
+                            de investigación.</p>
+                    </div>
+
+                    <div class="image-container">
+                        <img class="image-container" src="./img/home-img.webp" alt="" srcset="">
+                    </div>
+                </div>
+
+                <h2 class="section-title">Nuestro objetivo</h2>
+                <div class="text-content">
+                <p>Desarrollar un sistema web responsivo que facilite la gestión y el seguimiento de los proyectos del Instituto Tecnológico Superior de Felipe Carrillo Puerto.</p>
+                <p>Buscamos automatizar procesos como la entrega de reportes y la organización de la información del personal, docentes y estudiantes, aprovechando el uso de tecnologías web y bases de datos modernas.</p>
+            </div>
+</div>
+        </div>
+</body>
+
+</html>
