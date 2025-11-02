8a07b385b9baeeeab62f6adb7ee7320c647ab06a
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2025-11-01 22:09:31 -0500

    Update header.php

--- publico/incluido/header.php
+++ publico/incluido/header.php
@@ -1,1 +1,7 @@
-
+<div class="header">
+    <div class="logo">ITSFCP/PROYECTOS</div>
+    <div class="header-right">
+        <input type="text" class="search-box" placeholder="Buscar">
+        <button class="login-btn">Iniciar sesión</button>
+    </div>
+</div>
