a10a5324c3c5f12df2d96e5474a13f5d20459f1a
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2025-12-03 11:54:02 -0500

    Create descargar_guia.php
    Se añade código.

---
+++ Vistas/Tareas/descargar_guia.php
@@ -0,0 +1,18 @@
+<?php
+require "../ITSFCP-PROYECTOS/publico/config/conexion.php";
+
+$id = $_GET['id'];
+
+$sql = "SELECT archivo_guia, archivo_nombre, archivo_tipo 
+        FROM tbl_seguimiento 
+        WHERE id_avances = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$file = $stmt->get_result()->fetch_assoc();
+
+header("Content-Type: " . $file['archivo_tipo']);
+header("Content-Disposition: attachment; filename=" . $file['archivo_nombre']);
+echo $file['archivo_guia'];
+exit;
