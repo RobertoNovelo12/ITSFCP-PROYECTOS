de9a388ca00a32450d5b02c72b710c7918667392
RobertoNovelo12 <robertonovelo98@gmail.com>
2025-12-07 13:07:20 -0500

    Add custom modal styles and functionality to enhance user interaction

--- layout.php
+++ layout.php
@@ -29,11 +29,10 @@
             }
         })();
     </script>
-    
-    <link rel="stylesheet" href="/ITSFCP-PROYECTOS/publico/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/ITSFCP-PROYECTOS/publico/css/styles.css">
 
 </head>
 
