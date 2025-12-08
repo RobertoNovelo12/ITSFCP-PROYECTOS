5950c76eb046bb3244b80d93bf341fc665c125af
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2025-12-08 13:35:05 -0500

    Update tarea.php
    Correcciones.

--- Vistas/Tareas/tarea.php
+++ Vistas/Tareas/tarea.php
@@ -12,79 +12,110 @@ if (!isset($_SESSION['id_usuario'])) {
 $rol = $_SESSION['rol'];
 $id = $_SESSION['id_usuario'];
 
-$id_asignacion = $_GET["id_asignacion"] ?? null;
-$tipo = $_GET["tipo"] ?? null;
-$id_proyecto = $_GET["id_proyectos"] ?? null;
-$action = $_POST['action'] ?? null;
+$id_asignacion = $_POST["id_asignacion"]
+    ?? $_GET["id_asignacion"]
+    ?? null;
+$id_tarea      = $_POST["id_tarea"] ?? $_GET["id_tarea"] ?? null;
 
+if ($id_asignacion == null) {
+    die("ERROR: No se recibió id_asignacion");
+}
+
+$id_proyecto = $_GET["id_proyectos"] ?? null;
+$action = $_POST['action'] ?? $_GET['action'] ?? null;
+$tipo = $_GET['tipo'] ?? null;
 require_once '../../Controladores/tareasControlador.php';
 $tareaControlador = new TareaControlador();
 
-//Datos necesarios
-$datos = $tareaControlador->mostrarTarea($id_asignacion, $rol); // Para rellenar
-if ($action == 'editarTarea') {
-    $tareaControlador->editarTarea($_POST, $id, $rol);
+// DATOS
+$datos = $tareaControlador->mostrarTarea($id_asignacion, $rol);
+
+if ($action == 'editarTareaEstudiante') {
+    $tareaControlador->editarTareaEstudiante($_POST, $rol);
 }
 if ($action == 'editarTareaRevisar') {
-    $tareaControlador->editarTareaRevisar($_POST, $id, $rol);
+    $tareaControlador->editarTareaRevisar($_POST, $rol);
 }
+if ($action == 'actualizarestado') {
+    $tareaControlador->actualizarestado($_GET['id_tarea'], $rol, $_GET['tipo'], $id_proyecto, $id_asignacion);
+}
+
 // ======================
-// GENERAR CONTENIDO
+// GENERAR VISTA
 // ======================
 ob_start();
 ?>
 <div class="container-fluid py-4">
     <div class="row mb-3 align-items-center">
+
         <div class="row mb-1">
             <div class="col-6">
                 <h3>Revisar Tarea</h3>
             </div>
             <div class="col-6 text-end">
-                <a href="lista_tareas.php?id_tarea=<?= $datos['id_tarea']; ?>&id_proyectos=<?= $id_proyecto; ?>" class="btn btn-danger">Regresar</a>
+                <?php if ($rol == "investigador"){ ?>
+                    <a href="lista_tareas.php?id_tarea=<?= $id_tarea; ?>&id_proyectos=<?= $id_proyecto; ?>" class="btn btn-danger">Regresar</a>
+                <?php }elseif ($rol == "estudiante"){ ?>
+                    <a href="tareas_estudiante.php?id_tarea=<?= $id_tarea; ?>&id_proyectos=<?= $id_proyecto; ?>" class="btn btn-danger">Regresar</a>
+                <?php } ?>
             </div>
-            <div class="row mb-1">
-                <div class="mb-3">
-                    <h5>Descripción</h5>
-                    <span><?= $datos['descripcion'] ?></span>
-                </div>
-            </div>
-            <div class="row mb-1">
-                <div class="mb-3">
-                    <h5>Instrucciones</h5>
-                    <span><?= $datos['instrucciones'] ?></span>
-                </div>
-            </div>
-
-            <form action="tarea.php" method="POST" enctype="multipart/form-data">
-                <div class="row mb-1">
-                    <?php if ($rol == "estudiante"): ?>
-                        <input type="hidden" name="action" value="editarTarea">
-                    <?php endif; ?>
-                    <?php if ($rol == "investigador"): ?>
-                        <input type="hidden" name="action" value="editarTareaRevisar">
-                    <?php endif; ?>
-                    <input type="hidden" name="id_tarea[]" value="<?= $datos['id_tarea'] ?>">
-
-                    <?= $tareaControlador->tareas($tipo, $rol, $datos) ?>
-                </div>
-
-                <div class="row mb-1">
-                    <?php if ($rol == "investigador"): ?>
-                        <a href="editar.php?id_tarea=<?php $id_tarea; ?>&action=editarTareaRevisar&tipo=Corregir" type="button" class="btn btn-info" data-bs-toggle="tooltip" data-bs-placement="top"
-                                data-bs-custom-class="custom-tooltip" data-bs-title="Solicitar corregir">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z" />
-                                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466" />
-                                    </svg></a>
-                    <?php endif; ?>
-                    <?php if ($rol != "supervisor"): ?>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Guardar cambios</button>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </form>
         </div>
+
+        <div class="row mb-1">
+            <div class="mb-3">
+                <h5>Descripción</h5>
+                <span><?= $datos['descripcion'] ?? "" ?></span>
+            </div>
+        </div>
+
+        <div class="row mb-1">
+            <div class="mb-3">
+                <h5>Instrucciones</h5>
+                <span><?= $datos['instrucciones'] ?? "" ?></span>
+            </div>
+        </div>
+
+        <form action="tarea.php" method="POST" enctype="multipart/form-data">
+            <div class="row mb-1">
+
+                <?php if ($rol == "estudiante"): ?>
+                    <input type="hidden" name="action" value="editarTareaEstudiante">
+                <?php endif; ?>
+
+                <?php if ($rol == "investigador"): ?>
+                    <input type="hidden" name="action" value="editarTareaRevisar">
+                <?php endif; ?>
+
+                <input type="hidden" name="id_tarea" value="<?= $datos['id_tarea']; ?>">
+                <input type="hidden" name="id_asignacion" value="<?= $datos['id_asignacion']; ?>">
+
+                <?php echo $tareaControlador->tareas($datos['tipo_tarea'], $rol, $datos) ?? ""; ?>
+            </div>
+
+            <div class="row mb-1">
+                <div class="col-12">
+
+                    <?php if ($rol == "investigador"): ?>
+                        <a href="tarea.php?id_tarea=<?= $datos['id_tarea']; ?>&id_asignacion=<?= $id_asignacion; ?>&id_proyectos=<?= $id_proyecto; ?>&action=actualizarestado&tipo=Aprobado"
+                            class="btn btn-success">Aprobar tarea</a>
+                        <a href="tarea.php?id_tarea=<?= $datos['id_tarea']; ?>&id_asignacion=<?= $id_asignacion; ?>&id_proyectos=<?= $id_proyecto; ?>&action=actualizarestado&tipo=Corregir"
+                            class="btn btn-info">Solicitar corregir</a>
+
+                    <?php endif; ?>
+
+                    <?php if ($rol == "estudiante"): ?>
+                        <a href="tarea.php?id_tarea=<?= $datos['id_tarea']; ?>&id_asignacion=<?= $id_asignacion; ?>&id_proyectos=<?= $id_proyecto; ?>&action=actualizarestado&tipo=Revisar"
+                            class="btn btn-success">Enviar tarea</a>
+                    <?php endif; ?>
+
+                    <?php if ($rol != "supervisor"): ?>
+                        <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+        </form>
+
     </div>
 </div>
 
