34bfae6317d23bf1639c5ba083f651d11b9a2abe
LIX1313 <94031525+LIX1313@users.noreply.github.com>
2025-12-09 12:39:41 -0500

    Update comentarios.php
    Correcciones.

--- Vistas/Proyectos/comentarios.php
+++ Vistas/Proyectos/comentarios.php
@@ -11,63 +11,96 @@ if (!isset($_SESSION['id_usuario'])) {
 }
 $rol = $_SESSION['rol'];
 $id = $_SESSION['id_usuario'];
-$action = $_POST['action'] ?? null;
 
-$id_proyectos = $_GET['id_proyectos'] ?? null;
-$motivo = $_GET['motivo'] ?? null;
+$id_tarea = $_GET["id_tarea"] ?? null;
+$id_proyectos = $_GET["id_proyectos"] ?? $_POST["id_proyectos"] ?? null;
+$action = $_POST['action'] ?? $_GET['action'] ?? null;
 
-if ($motivo == "cierre_rechazado") {
-    $texto_motivo = "Cierre rechazado";
-} else if ($motivo == "creacion_rechazada") {
-    $texto_motivo = "Cierre rechazado";
+
+require_once '../../Controladores/tareasControlador.php';
+$tareaControlador = new TareaControlador();
+
+//Datos necesarios
+$tarea = $tareaControlador->mostrarEditarTarea($id_tarea, $rol); // Para rellenar
+
+if ($action == 'editarTarea') {
+    $tareaControlador->editarTarea($_POST, $rol, $id_proyectos);
 }
-//Se llama al controlador
-
-require_once '..\..\Controladores\proyectoControlador.php';
-
-$proyectoControlador = new ProyectoControlador();
-
-// Actualización de estados
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && ($_POST['action'] ?? '') == 'actualizarestadoRechazo' && $rol == "supervisor") {
-    $proyectoControlador->actualizarestadoRechazo($_POST, $id, $rol);
+if ($action == 'actualizarestado' && isset($_GET['id_tarea'])) {
+    $tareaControlador->actualizarestado($_GET['id_tarea'], $rol, $_GET['tipo'], $id_proyectos);
 }
+
+// ======================
+// GENERAR CONTENIDO
+// ======================
 ob_start();
 include __DIR__ . '/../../mensaje.php';
 ?>
+
 <div class="container-fluid py-4">
     <div class="row mb-3 align-items-center">
         <div class="row mb-1">
             <div class="col-6">
-                <h3>Comentario</h3>
+                <h3>Editar Tarea</h3>
             </div>
-            <div class="col-6 col-md-6 text-md-end mb-2 mb-md-0 text-end">
-                <a href="tabla.php" class="btn btn-danger w-100 w-md-auto">Regresar</a>
+            <div class="col-6 text-end">
+                <a href="tabla.php?id_proyectos=<?= $id_proyectos; ?>" class="btn btn-danger">Regresar</a>
             </div>
-            <form method="POST" action="comentarios.php">
+
+            <form action="editar.php?id_proyectos=<?php $id_proyectos ?? "" ?>" method="POST" enctype="multipart/form-data">
                 <div class="row mb-1">
+                    <input type="hidden" name="action" value="editarTarea">
+                    <input type="hidden" name="id_tarea" value="<?= $tarea['id_tarea']; ?>">
+                    <input type="hidden" name="id_proyectos" value="<?= $id_proyectos; ?>">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Motivo</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" value="<?php echo $texto_motivo ?? null; ?>" disabled>
+                        <label>Descripción:</label>
+                        <textarea name="descripcion" class="form-control" rows="6"><?= htmlspecialchars($tarea['descripcion']) ?></textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">Comentario</label>
-                        <textarea class="form-control" name="comentario" id="InputFormLimpiar2" rows="3" required></textarea>
+                        <label>Instrucciones:</label>
+                        <textarea name="instrucciones" class="form-control" rows="6"><?= htmlspecialchars($tarea['instrucciones']) ?></textarea>
                     </div>
-                    <input type="hidden" name="tipo" value="<?php echo $motivo; ?>">
-                    <input type="hidden" name="action" value="actualizarestadoRechazo">
-                    <input type="hidden" name="id_proyectos" value="<?php echo $id_proyectos; ?>">
+
                     <div class="row mb-1">
-                        <div class="col-12 text-center">
-                            <button type="submit" class="btn btn-danger">Confirmar</button>
+                        <div class="col-md">
+                            <div class="mb-3">
+                                <label>Fecha entrega:</label>
+                                <input type="date" name="fecha_entrega" class="form-control"
+                                    value="<?= $tarea['fecha_entrega']; ?>">
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="mb-3">
+                                <label>Archivo actual:</label>
+                                <?php if (!empty($tarea) && !empty($tarea['archivo_nombre'])): ?>
+                                    <a href="descargar_guia.php?id=<?= $tarea['id_tarea'] ?>">
+                                        Descargar archivo (<?= $tarea['archivo_nombre']; ?>)
+                                    </a>
+                                <?php else: ?>
+                                    <p>No hay archivo cargado.</p>
+                                <?php endif; ?>
+
+                                <label>Subir archivo nuevo:</label>
+                                <input type="file" name="archivo">
+                            </div>
                         </div>
                     </div>
+                    <div class="row mb-1">
+                        <div class="col-12">
+                            <div class="col-12">
+                                <?php echo $tareaControlador->botonesAccionTarea($tarea['id_tarea'], $rol, $tarea['estado'], $id_proyectos); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
 </div>
+
 <?php
 $contenido = ob_get_clean();
-$titulo = "Comentarios";
+$titulo = "Editar tarea";
 $bodyClass = "proyectos-page";
 
 include __DIR__ . '/../../layout.php';
